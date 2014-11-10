c61578cddbb5e8eda11ccf70f1e4e12deda821e5
Neil Laurance <neil.laurance@gmail.com>
2014-11-10 01:11:30 +0000

    changes for queen_attack

--- queen-attack/queen_attack.clj
+++ queen-attack/queen_attack.clj
@@ -24,3 +24,23 @@
         row-sets (map set board)
         col-sets (map set (apply map vector board))]
     nil))
+
+(defn row-sets [board]
+  (map set board))
+
+(defn column-sets [board]
+  (map set (apply map vector board)))
+
+(def test-board
+  [[000 001 002 003 004 005 006 007]
+   [010 011 012 013 014 015 016 017]
+   [020 021 022 023 024 025 026 027]
+   [030 031 032 033 034 035 036 037]
+   [040 041 042 043 044 045 046 047]
+   [050 051 052 043 054 055 056 057]
+   [060 061 062 043 064 065 066 067]
+   [070 071 072 043 074 075 076 077]])
+
+(row-sets test-board)
+(column-sets test-board)
+(right-diagonals test-board)
--- queen-attack/queen_attack_test.clj
+++ queen-attack/queen_attack_test.clj
@@ -37,4 +37,3 @@
   (is (= true  (queen-attack/can-attack {:w [4 1] :b [6 3]}))))
 
 (run-tests)
-
