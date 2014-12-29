b939b198e4f03091a75a6fd93d102294d8c7e16e
Neil Laurance <neil.laurance@gmail.com>
2014-12-29 20:24:00 +0000

    Added missing clj-time dependencies

--- clojure/meetup/project.clj
+++ clojure/meetup/project.clj
@@ -1,4 +1,5 @@
 (defproject meetup "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [clj-time "0.9.0"]]
   :source-paths ["."]
   :test-paths ["."])
